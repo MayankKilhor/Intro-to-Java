ea285b08a33af24a86c4ea30c83d94dba4be9b65
Rory William <64885374+TheRoryWillAim@users.noreply.github.com>
2021-05-28 13:16:00 +0530

    Update toggle_bulb.java

--- Math_Problems/Toggle_the_Bulb/toggle_bulb.java
+++ Math_Problems/Toggle_the_Bulb/toggle_bulb.java
@@ -1,35 +1,12 @@
-import java.util.Scanner;
-class Main
-{
-  public static void main(String args[])
-  {
-    //Try out your code here
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    sc.close();
-    int[] a= new int[n];
-    for(int i=0;i<n;i++){
-      a[i]=1;
-    }
-    for(int i=1;i<n-1;i++){
-      for(int j=i;j<n;j=j+i+1){
-        if(a[j]==0){
-          a[j]=1;
-    }else{a[j]=0;}
-      }
-    }
-	if(a[n-1]==0){
-      a[n-1]=1;
-    }else{
-      a[n-1]=0;
-    }
-    int sum=0;
-    for(int i=0;i<n;i++)
-    {
-      if(a[i]==1){
-        sum+=1;
-      }
-    }
-    System.out.println(sum);
-  }
+import java.util.*;
+
+
+class Main{
+
+public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println((int)Math.sqrt(n));
+		
+	}
 }
