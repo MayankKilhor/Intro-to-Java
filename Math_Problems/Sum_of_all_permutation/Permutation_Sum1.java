3f1db07fbfba789a5a45b017ad5f47db14a6af23
Rory William <64885374+TheRoryWillAim@users.noreply.github.com>
2021-05-29 12:34:12 +0530

    sum of permutation 1

---
+++ Math_Problems/Sum_of_all_permutation/Permutation_Sum1.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+class Main 
+{
+	//Sum of all possible permutations O(logn)
+    static long fac(int n)
+    {
+        long f = 1 ;
+        for(int i = n ; i>1 ; i--)
+            f *= i ;
+        
+        return f ;
+    }
+    public static void main (String[] args) 
+	{
+        int n , temp ;
+        Scanner sc = new Scanner(System.in) ;
+        n = sc.nextInt() ;
+        temp = n ;
+            
+        long p = 1 ;
+        int a , i , sum = 0 ;
+            
+        for(i=0 ; i<n ; i++)
+        {
+            a = sc.nextInt() ;
+            sum += a ;
+        }
+        while(--n!=0)
+            p = p*10 + 1 ;
+            
+        System.out.print(sum*fac(temp-1)*p);
+	}
+}
---
+++ Math_Problems/Sum_of_all_permutation/README.md
@@ -0,0 +1,38 @@
+# Sum of all permutations
+## Given the size N of array A, followed by the line of array elements. Find the sum of all possible permutation values without duplicates.
+
+
+
+Constraints:
+
+1<N<9
+
+0<Ai<10
+
+
+
+Sample Input1:
+
+2
+
+1 2
+
+Sample Output1:
+
+33
+
+Explanation:
+
+The possible permutations are 12 and 21. Their sum will be 33.
+
+
+
+Sample Input2:
+
+3
+
+1 2 3
+
+Sample Output2:
+
+1332
