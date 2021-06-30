de3a303c76e4636775c4428a64eaa507f1330657
Rory William <64885374+TheRoryWillAim@users.noreply.github.com>
2021-06-30 11:47:47 +0530

    linkedlist

---
+++ Datastructures/List/InsertAnElementInLinkedList/README.md
@@ -0,0 +1,40 @@
+# Insert an element in linked list
+## Write a program to insert an element at the end of the linked list. Get the values continuously from the user until the user enters a negative value.
+
+
+
+Format:
+
+Input:
+
+The input should be numbers (until user enters a negative value).
+
+Output:
+
+The output should be the list elements in separate lines.
+
+
+
+Example:
+
+Input:
+
+1
+
+2
+
+3
+
+4
+
+-1
+
+Output:
+
+1
+
+2
+
+3
+
+4
---
+++ Datastructures/List/InsertAnElementInLinkedList/insert.java
@@ -0,0 +1,54 @@
+import java.util.*;
+class LinkedList
+{
+    Node head;
+    static class Node{
+        int data;
+        Node next;
+        public Node(int data)
+        {
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public LinkedList append(LinkedList list,int data)
+    {
+        Node newnode=new Node(data);
+        Node temp=head;
+        if(list.head==null)
+            list.head=newnode;
+        else
+        {
+            while(temp.next!=null)
+                temp=temp.next;
+            temp.next=newnode;
+        }
+        return list;
+    }
+    public void display()
+    {
+        Node temp=head;
+        while(temp!=null)
+        {
+            System.out.println(temp.data);
+            temp=temp.next;
+        }
+    }
+    
+}
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		LinkedList list=new LinkedList();
+		int data;
+		do
+		{
+		    data=s.nextInt();
+		    if(data>=0)
+		        list=list.append(list,data);
+		}while(data>=0);
+		list.display();
+		
+	}
+}
